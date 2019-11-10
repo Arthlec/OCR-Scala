2a37cf6a87ae6892169cf8769576890f50a72a43
Artlec <38135800+Arthlec@users.noreply.github.com>
2019-11-10 17:35:13 +0100

    Update doc

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,21 +1,18 @@
 /***
  * Optical Character Recognition (OCR)
  *
- * reconnaissance de lettres ou caractères avec un certain nombre de points
- * Matlab pour produire des lettres avec du bruit
- * classifier classique plutôt que le réseau de neurones
  *
  * Datasets :
  * https://lionbridge.ai/datasets/15-best-ocr-handwriting-datasets/
  *
- *    Stanford Dataset (http://ai.stanford.edu/~btaskar/ocr/)
+ * *** Stanford Dataset (http://ai.stanford.edu/~btaskar/ocr/) ***
  * The tab delimited data file (letter.data.gz) contains a line for each letter, with its label, pixel values,
  * and several additional fields listed in letter.names file.
  * Columns : id, letter, next_id, word_id, position, fold, p_0_0, p_0_1, ...
  * Lettres en 16 x 8 pixels
  *
  *
- *    The MNIST Database (http://yann.lecun.com/exdb/mnist/)
+ * *** The MNIST Database (http://yann.lecun.com/exdb/mnist/) ***
  * The MNIST database of handwritten digits, available from this page, has a training set of 60,000 examples, and a test
  * set of 10,000 examples. It is a subset of a larger set available from NIST. The digits have been size-normalized and
  * centered in a fixed-size image.
@@ -24,6 +21,7 @@
  * used by the normalization algorithm. the images were centered in a 28x28 image by computing the center of mass of the
  * pixels, and translating the image so as to position this point at the center of the 28x28 field.
  *
+ * Getting the dataset as a PNG format (https://github.com/myleott/mnist_png)
  *
  *
  * Ressources :
@@ -31,9 +29,11 @@
  * https://github.com/saibot94/scala-ocr-parser
  * http://tess4j.sourceforge.net/
  * https://towardsdatascience.com/ocr-with-akka-tesseract-and-javacv-part-1-702781fc73ca
- *
  * https://towardsdatascience.com/a-gentle-introduction-to-ocr-ee1469a201aa
- * https://exercism.io/tracks/scala/exercises/ocr-numbers/solutions/5c2ed700a4a94eb1acee26157ae224b2
+ *
+ * Scala MNIST :
+ * https://github.com/alno/scalann/blob/master/examples/src/main/scala/org/scalann/examples/Mnist.scala
+ * https://github.com/saurfang/spark-knn/blob/master/spark-knn-examples/src/main/scala/com/github/saurfang/spark/ml/knn/examples/MNIST.scala
  */
 
 object Main extends App with OCR {
