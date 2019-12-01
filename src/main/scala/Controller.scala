959d8e1fb16aa9db457ecff26d78f426b371b64f
Artlec <38135800+Arthlec@users.noreply.github.com>
2019-12-01 18:28:51 +0100

    Add Binarization

--- src/main/scala/Controller.scala
+++ src/main/scala/Controller.scala
@@ -3,21 +3,54 @@ package main.scala
 import java.io.File
 
 import net.sourceforge.tess4j.{ITesseract, Tesseract}
-import scalafx.scene.image.Image
+import scalafx.embed.swing.SwingFXUtils
+import scalafx.scene.image.{Image, PixelReader, WritableImage}
+import scalafx.scene.paint.Color
 
 object Controller {
   def gray2binary(image : Image): Image = {
-    val pixelReader = image.pixelReader
-    pixelReader
-    image
+    val height = image.getHeight.toInt
+    val width = image.getWidth.toInt
+
+    val pixelReader = image.pixelReader.get
+    val writableImage : WritableImage = new WritableImage(pixelReader, width, height)
+    val pixelWriter = writableImage.pixelWriter
+
+    val pixelsValue = for {
+      y <- 0 until height
+      x <- 0 until width
+    } yield {
+      val color = pixelReader.getColor(x, y)
+      val grayColor = color.grayscale
+      val componentValue = (grayColor.blue * 255).toInt // or red or green
+      componentValue
+    }
+
+    val threshold = pixelsValue.foldLeft((0.0, 1)) { case ((avg, idx), next) => (avg + (next - avg)/idx, idx + 1) }._1
+
+    // Mandatory for loop to use pixelReader object
+    for {
+      y <- 0 until height
+      x <- 0 until width
+    }  {
+      val color = pixelReader.getColor(x, y)
+      val grayColor = color.grayscale
+      val componentValue = (grayColor.blue * 255).toInt // or red or green
+      if (componentValue > threshold){
+        pixelWriter.setColor(x, y, Color.White)
+      }else{
+        pixelWriter.setColor(x, y, Color.Black)
+      }
+    }
+    writableImage
   }
 
-  def ocr(imagePath : String): String = {
+  def ocr(image : Image): String = {
     val tesseract: ITesseract = new Tesseract
     tesseract.setDatapath("src/main/data/tessdata")
     tesseract.setLanguage("eng")
     tesseract.setTessVariable("user_defined_dpi", "300")
-    val text = tesseract.doOCR(new File("src" + imagePath))
+    val text = tesseract.doOCR(SwingFXUtils.fromFXImage(image, null))
     text
   }
 }
--- src/main/scala/View.scala
+++ src/main/scala/View.scala
@@ -12,28 +12,31 @@ import scalafx.scene.image.Image
 import scalafx.scene.layout.{AnchorPane, VBox}
 import scalafx.scene.paint.Color.Black
 import main.scala.Controller.ocr
+import main.scala.Controller.gray2binary
 
 object View {
   def refresh(windowWidth : Double, windowHeight : Double, canvasOriginal : Canvas, canvasProcessed : Canvas,
               imagesIterator: Iterator[Path]):
   PrimaryStage = {
-    val graphicsContextOriginal: GraphicsContext = canvasOriginal.graphicsContext2D
-    val graphicsContextProcessed: GraphicsContext = canvasProcessed.graphicsContext2D
-    val imagePath: String = imagesIterator.next.toString.replace("\\", "/")
-    val imageRelPath: String = imagePath.substring(imagePath.lastIndexOf("/main"), imagePath.length)
-//    System.out.println(imagePath)
-    System.out.println(imageRelPath)
-    val image = new Image(imageRelPath, canvasOriginal.getWidth, canvasOriginal.getHeight, true, false)
-
     new PrimaryStage {
+      val graphicsContextOriginal: GraphicsContext = canvasOriginal.graphicsContext2D
+      val graphicsContextProcessed: GraphicsContext = canvasProcessed.graphicsContext2D
+      val imagePath: String = imagesIterator.next.toString.replace("\\", "/")
+      val imageRelPath: String = imagePath.substring(imagePath.lastIndexOf("/main"), imagePath.length)
+      //    System.out.println(imagePath)
+      System.out.println(imageRelPath)
+      val image = new Image(imageRelPath, canvasOriginal.getWidth, canvasOriginal.getHeight, true, false)
+      val imageProcessed: Image = gray2binary(image)
+
       title = "Scala OCR"
       resizable = false
       width = windowWidth
       height = windowHeight
+
       scene = new Scene {
         fill = Black
         root = new VBox {
-          val outputText: Label = new Label {text = "Output : " + ocr(imageRelPath)}
+          val outputText: Label = new Label {text = "Output : " + ocr(imageProcessed)}
           val previousButton: Button = new Button {
             text = "Previous"
             disable = true
@@ -68,7 +71,7 @@ object View {
       graphicsContextOriginal.clearRect(0,0, canvasOriginal.getWidth, canvasOriginal.getHeight)
       graphicsContextOriginal.drawImage(image, 0, 0)
       graphicsContextProcessed.clearRect(0,0, canvasProcessed.getWidth, canvasProcessed.getHeight)
-      graphicsContextProcessed.drawImage(image, 0, 0)
+      graphicsContextProcessed.drawImage(imageProcessed, 0, 0)
     }
   }
 }
